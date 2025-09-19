20506bc68c8ff5c6d6ddde21dbbb68fd56e854e5
Adityuhh11 <saipremaditya@gmail.com>
2025-09-19 20:08:54 +0200

    feat: add user and blog models with bcrypt password hashing

---
+++ Back-end/src/model/blog.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+ 
+const blogSchema =  new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    content:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User"
+        
+    }
+})
---
+++ Back-end/src/model/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true , 'password is required']
+    },
+    refreshToken:{
+        type:String
+    }
+
+})
+
+userSchema.pre("save",async function(next){
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password,10)
+    }
+    next()
+})
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+export const User = mongoose.model("User",userSchema)
