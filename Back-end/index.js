40a907eab22d5967fd424df4d8b69408a9884cd7
Adityuhh11 <saipremaditya@gmail.com>
2025-09-25 12:43:12 +0200

    feat: setting up the server to serve static html files and access

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./db/index.db.js";
+import connectDB from "../Back-end/src/db/index.db.js";
+import path from "path"
 dotenv.config();
 const app = express()
 
+
+const __dirname = path.resolve(); // Get the absolute path of the project
+app.use(express.static(path.join(__dirname, '../Front-end')));
+
+
 connectDB()
 .then(()=>{
-    app.listen(process.env.PORT,(()=>{
+    app.listen(process.env.PORT|| 3000,(()=>{
         console.log(`Server is running on port ${process.env.PORT}`)
     }))
 })
