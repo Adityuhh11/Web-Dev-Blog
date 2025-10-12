6033734e8c281e48458fba6f8de546ebf04d6aa5
Adityuhh11 <saipremaditya@gmail.com>
2025-10-12 21:10:15 +0200

    chore: clean the code and install dependancies

--- Blog-Project/src/db/index.db.js
+++ Blog-Project/src/db/index.db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const connectDB = async()=>{
-      console.log("Connecting to MongoDB with URI:", process.env.MONGODB_URI);
     try {
         const connectionInstance = await mongoose.connect(process.env.MONGODB_URI)
         console.log(`MongoDB connected: ${connectionInstance.connection.host}`)
