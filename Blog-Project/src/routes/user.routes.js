d47a1a0ec16f4840dada96c70979f91d315746b7
Adityuhh11 <saipremaditya@gmail.com>
2025-10-12 21:11:47 +0200

    feat:implment login and logout routes

--- Blog-Project/src/routes/user.routes.js
+++ Blog-Project/src/routes/user.routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import {verifyJWT} from "../middleware/auth.middleware.js";
+import { loginUser } from "../controllers/user.controller.js";
 
 
 const router = Router();
@@ -6,3 +8,13 @@ const router = Router();
 router.get("/login",(req,res)=>{
     res.render("login")
 })
+router.post("/login", loginUser);
+
+// Logout route
+router.post("/logout", (req, res) => {
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
+    res.redirect('/login');
+});
+
+export default router;
